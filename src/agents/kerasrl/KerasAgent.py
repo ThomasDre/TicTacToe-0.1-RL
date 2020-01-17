691f5fa22f18509f03b987ba336896697a8d2dad
Thomas Drescher <drescher.thomas@outlook.com>
2020-01-17 15:38:12 +0100

    Complete 1st Dummy Keras Agent
    And also the infamously bad weight are included :)

--- src/agents/kerasrl/KerasAgent.py
+++ src/agents/kerasrl/KerasAgent.py
@@ -1,6 +1,11 @@
 from src.agents.base.Agent import RlAgent
 from custom_env.tictactoe import strategy
 
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+from rl.agents import SARSAAgent
+from rl.policy import EpsGreedyQPolicy
+
 
 class KerasAgent(RlAgent):
     """
@@ -15,25 +20,35 @@ class KerasAgent(RlAgent):
         self.env.set_opponent(strategy.random_bot)
 
     def run(self):
-        for episode in range(100):
-            self.env.reset()
-            done = False
-
-            while not done:
-                # silly agent chooses random actions until he chooses a valid one, and then he hopes for the best
-                while True:
-                    _, _, done, info = self.env.step(self.env.get_sampled_action())
-                    invalid = info['illegal']
-
-                    if not invalid:
-                        break
-
-                self.env.render()
-
-        self.env.close()
+        model = self.agent()  # agent(3, 2)
+        policy = EpsGreedyQPolicy()
+        sarsa = SARSAAgent(model=model, policy=policy, nb_actions=9)
+        sarsa.compile('adam', metrics=['mse'])
+        sarsa.fit(env=self.env, nb_steps=1000, visualize=False, verbose=1)
+        sarsa.save_weights('../../../training/keras/keras-sarsa_base_50000.h5f', True)
 
     def action(self):
         pass
+
+    @staticmethod
+    def agent():
+        """
+
+        Consider that this class is currently not completed.
+        For any state, action representation coming in, the best available response is picked from the trained
+        network.
+
+
+        :param states: the number of states the environment can be in
+        :param actions: the number of available action at each timestep
+        :return:
+        """
+        model = Sequential()
+        model.add(Flatten(input_shape=(1, 3, 3)))
+        model.add(Dense(16, activation='relu'))
+        model.add(Dense(9, activation='softmax', input_shape=(1, 3, 3)))
+
+        return model
 
 
 if __name__ == '__main__':
@@ -41,4 +56,15 @@ if __name__ == '__main__':
     agent.run()
     print(agent.env.observation_space)
     print(agent.env.action_space)
+    print("Samples:")
+    print(agent.env.observation_space.sample())
+    print(agent.env.action_space.sample())
+    print("Diagnostic")
+    print(agent.env.observation_space.shape)
+    print(agent.env.observation_space.shape[0])
+    print(agent.env.action_space)
+
+    print("Look at the model")
+
+
 
